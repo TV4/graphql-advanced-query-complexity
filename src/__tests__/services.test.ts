d9516b07cab4193ce4dcd5d39efcdddf176ff0c0
Emil Oberg <emil@bitupnorth.com>
2022-06-16 21:22:50 +0200

    Add service concept to fields

--- src/__tests__/services.test.ts
+++ src/__tests__/services.test.ts
@@ -20,136 +20,346 @@ const calculators = [
   fieldCalculator({ directive: createFieldDirective() }),
 ];
 
-it('called once with cost', async () => {
-  const baseSchema = gql`
-    ${fieldDirectiveSDL}
-    ${objectDirectiveSDL}
+describe('object directive', () => {
+  it('called once with cost', async () => {
+    const baseSchema = gql`
+      ${fieldDirectiveSDL}
+      ${objectDirectiveSDL}
 
-    type Query {
-      test(amount: Int = 5): [Obj] @complexity(multiplier: "amount")
-    }
-
-    type Obj @objComplexity(services: ["engagement"]) {
-      string: String
-    }
-  `;
-
-  const query = gql`
-    query {
-      test(amount: 4) {
-        string
+      type Query {
+        test(amount: Int = 5): [Obj] @complexity(multiplier: "amount")
       }
-    }
-  `;
 
-  const schema = makeExecutableSchema({ typeDefs: [baseSchema] });
-  const validationResults = await validateGraphQlDocuments(schema, [{ document: query }]);
-  expect(validationResults).toEqual([]);
+      type Obj @objComplexity(services: ["serviceX"]) {
+        string: String
+      }
+    `;
 
-  const complexity = getComplexity({
-    calculators,
-    schema,
-    query,
-    postCalculations: [
-      createServicesPostCalculation({
-        engagement: {
-          calledOnce: true,
-          cost: 100,
-        },
-      }),
-    ],
+    const query = gql`
+      query {
+        test(amount: 4) {
+          string
+        }
+      }
+    `;
+
+    const schema = makeExecutableSchema({ typeDefs: [baseSchema] });
+    const validationResults = await validateGraphQlDocuments(schema, [{ document: query }]);
+    expect(validationResults).toEqual([]);
+
+    const complexity = getComplexity({
+      calculators,
+      schema,
+      query,
+      postCalculations: [
+        createServicesPostCalculation({
+          serviceX: {
+            calledOnce: true,
+            cost: 100,
+          },
+        }),
+      ],
+    });
+
+    expect(complexity.extra?.services.serviceX.value).toBe(4);
+    expect(complexity.cost).toBe(100);
   });
 
-  expect(complexity.extra?.services.engagement.value).toBe(4);
-  expect(complexity.cost).toBe(100);
-});
+  it('called multiple times with cost', async () => {
+    const baseSchema = gql`
+      ${fieldDirectiveSDL}
+      ${objectDirectiveSDL}
 
-it('called multiple times with cost', async () => {
-  const baseSchema = gql`
-    ${fieldDirectiveSDL}
-    ${objectDirectiveSDL}
-
-    type Query {
-      test(amount: Int = 5): [Obj] @complexity(multiplier: "amount")
-    }
-
-    type Obj @objComplexity(services: ["engagement"]) {
-      string: String
-    }
-  `;
-
-  const query = gql`
-    query {
-      test(amount: 4) {
-        string
+      type Query {
+        test(amount: Int = 5): [Obj] @complexity(multiplier: "amount")
       }
-    }
-  `;
 
-  const schema = makeExecutableSchema({ typeDefs: [baseSchema] });
-  const validationResults = await validateGraphQlDocuments(schema, [{ document: query }]);
-  expect(validationResults).toEqual([]);
+      type Obj @objComplexity(services: ["serviceX"]) {
+        string: String
+      }
+    `;
 
-  const complexity = getComplexity({
-    calculators,
-    schema,
-    query,
-    postCalculations: [
-      createServicesPostCalculation({
-        engagement: {
-          cost: 100,
-        },
-      }),
-    ],
+    const query = gql`
+      query {
+        test(amount: 4) {
+          string
+        }
+      }
+    `;
+
+    const schema = makeExecutableSchema({ typeDefs: [baseSchema] });
+    const validationResults = await validateGraphQlDocuments(schema, [{ document: query }]);
+    expect(validationResults).toEqual([]);
+
+    const complexity = getComplexity({
+      calculators,
+      schema,
+      query,
+      postCalculations: [
+        createServicesPostCalculation({
+          serviceX: {
+            cost: 100,
+          },
+        }),
+      ],
+    });
+
+    expect(complexity.extra?.services.serviceX.value).toBe(4);
+    expect(complexity.cost).toBe(400);
   });
 
-  expect(complexity.extra?.services.engagement.value).toBe(4);
-  expect(complexity.cost).toBe(400);
-});
+  it('max times, creates error', async () => {
+    const baseSchema = gql`
+      ${fieldDirectiveSDL}
+      ${objectDirectiveSDL}
 
-it('max times, creates error', async () => {
-  const baseSchema = gql`
-    ${fieldDirectiveSDL}
-    ${objectDirectiveSDL}
-
-    type Query {
-      test(amount: Int = 5): [Obj] @complexity(multiplier: "amount")
-    }
-
-    type Obj @objComplexity(services: ["engagement"]) {
-      string: String
-    }
-  `;
-
-  const query = gql`
-    query {
-      test(amount: 4) {
-        string
+      type Query {
+        test(amount: Int = 5): [Obj] @complexity(multiplier: "amount")
       }
-    }
-  `;
 
-  const schema = makeExecutableSchema({ typeDefs: [baseSchema] });
-  const validationResults = await validateGraphQlDocuments(schema, [{ document: query }]);
-  expect(validationResults).toEqual([]);
+      type Obj @objComplexity(services: ["serviceX"]) {
+        string: String
+      }
+    `;
 
-  const complexity = getComplexity({
-    calculators,
-    schema,
-    query,
-    postCalculations: [
-      createServicesPostCalculation({
-        engagement: {
-          cost: 100,
-          maxTimes: 3,
-        },
-      }),
-    ],
+    const query = gql`
+      query {
+        test(amount: 4) {
+          string
+        }
+      }
+    `;
+
+    const schema = makeExecutableSchema({ typeDefs: [baseSchema] });
+    const validationResults = await validateGraphQlDocuments(schema, [{ document: query }]);
+    expect(validationResults).toEqual([]);
+
+    const complexity = getComplexity({
+      calculators,
+      schema,
+      query,
+      postCalculations: [
+        createServicesPostCalculation({
+          serviceX: {
+            cost: 100,
+            maxTimes: 3,
+          },
+        }),
+      ],
+    });
+
+    expect(complexity.extra?.services.serviceX.value).toBe(4);
+    expect(complexity.cost).toBe(400);
+    expect(complexity.errors?.[0].message).toBe('Service serviceX may only be queried 3 times. Was queried 4 times');
   });
 
-  expect(complexity.extra?.services.engagement.value).toBe(4);
-  expect(complexity.cost).toBe(400);
-  expect(complexity.errors?.[0].message).toBe('Service engagement may only be queried 3 times. Was queried 4 times');
+  it('both normal cost and service cost', async () => {
+    const baseSchema = gql`
+      ${fieldDirectiveSDL}
+      ${objectDirectiveSDL}
+
+      type Query {
+        test: Obj
+      }
+
+      type Obj @objComplexity(services: ["serviceX"]) {
+        string: String @complexity(cost: 1)
+        number: Int @complexity(cost: 7)
+      }
+    `;
+
+    const query = gql`
+      query {
+        test {
+          string
+          number
+        }
+      }
+    `;
+
+    const schema = makeExecutableSchema({ typeDefs: [baseSchema] });
+    const validationResults = await validateGraphQlDocuments(schema, [{ document: query }]);
+    expect(validationResults).toEqual([]);
+
+    const complexity = getComplexity({
+      calculators,
+      schema,
+      query,
+      postCalculations: [
+        createServicesPostCalculation({
+          serviceX: {
+            cost: 100,
+            maxTimes: 3,
+          },
+        }),
+      ],
+    });
+
+    expect(complexity.cost).toBe(108); // 8 + 100
+  });
+
+  it('union', async () => {
+    const baseSchema = gql`
+      ${fieldDirectiveSDL}
+      ${objectDirectiveSDL}
+
+      type Query {
+        test(limit: Int): [Main] @complexity(multiplier: "limit")
+      }
+
+      union Main = First | Second
+
+      type ObjFirst @objComplexity(services: ["first"]) {
+        string: String
+      }
+
+      type ObjSecond @objComplexity(services: ["second"]) {
+        string: String
+      }
+
+      type First {
+        obj1: ObjFirst
+        obj2: ObjFirst
+        obj3: ObjFirst
+      }
+
+      type Second {
+        obj1: ObjSecond
+        obj2: ObjSecond
+        obj3: ObjSecond
+      }
+    `;
+
+    const query = gql`
+      query {
+        test(limit: 4) {
+          ... on First {
+            obj1 {
+              string
+            }
+            obj2 {
+              string
+            }
+            obj3 {
+              string
+            }
+          }
+          ... on Second {
+            obj1 {
+              string
+            }
+            obj2 {
+              string
+            }
+          }
+        }
+      }
+    `;
+
+    const schema = makeExecutableSchema({ typeDefs: [baseSchema] });
+    const validationResults = await validateGraphQlDocuments(schema, [{ document: query }]);
+    expect(validationResults).toEqual([]);
+
+    const complexity = getComplexity({
+      calculators,
+      schema,
+      query,
+    });
+
+    expect(complexity.cost).toBe(0);
+    expect(complexity.extra?.services.first.value).toBe(12); // 4 * 3
+    expect(complexity.extra?.services.second.value).toBe(8); // 4 * 2
+  });
 });
 
-it.todo('Should work on fields as well');
+describe('field directive', () => {
+  it('called multiple times with cost', async () => {
+    const baseSchema = gql`
+      ${fieldDirectiveSDL}
+      ${objectDirectiveSDL}
+
+      type Query {
+        test(amount: Int = 5): [Obj] @complexity(multiplier: "amount")
+      }
+
+      type Obj {
+        string: String @complexity(services: ["serviceX"])
+      }
+    `;
+
+    const query = gql`
+      query {
+        test(amount: 4) {
+          string
+        }
+      }
+    `;
+
+    const schema = makeExecutableSchema({ typeDefs: [baseSchema] });
+    const validationResults = await validateGraphQlDocuments(schema, [{ document: query }]);
+    expect(validationResults).toEqual([]);
+
+    const complexity = getComplexity({
+      calculators,
+      schema,
+      query,
+      postCalculations: [
+        createServicesPostCalculation({
+          serviceX: {
+            cost: 100,
+          },
+        }),
+      ],
+    });
+
+    expect(complexity.extra?.services.serviceX.value).toBe(4);
+    expect(complexity.cost).toBe(400);
+  });
+
+  it('called multiple times, multiple fields, with cost', async () => {
+    const baseSchema = gql`
+      ${fieldDirectiveSDL}
+      ${objectDirectiveSDL}
+
+      type Query {
+        test(amount: Int = 5): [Obj] @complexity(multiplier: "amount")
+      }
+
+      type Obj {
+        string: String @complexity(services: ["serviceX"])
+        string2: String @complexity(services: ["serviceX", "serviceY"])
+      }
+    `;
+
+    const query = gql`
+      query {
+        test(amount: 4) {
+          string
+          string2
+        }
+      }
+    `;
+
+    const schema = makeExecutableSchema({ typeDefs: [baseSchema] });
+    const validationResults = await validateGraphQlDocuments(schema, [{ document: query }]);
+    expect(validationResults).toEqual([]);
+
+    const complexity = getComplexity({
+      calculators,
+      schema,
+      query,
+      postCalculations: [
+        createServicesPostCalculation({
+          serviceX: {
+            cost: 100,
+          },
+          serviceY: {
+            cost: 20,
+          },
+        }),
+      ],
+    });
+
+    expect(complexity.cost).toBe(880); // 4 * 100 + 4 * 100  + 4 * 20
+  });
+});
--- src/calculators/fieldCalculator.ts
+++ src/calculators/fieldCalculator.ts
@@ -2,6 +2,7 @@ import { getDirectiveValues, GraphQLDirective } from 'graphql';
 
 import { ComplexityCalculator, Extra } from '..';
 import { isNumber, isString } from '../utils';
+import { ComplexityServices } from './objectCalculator';
 
 const getDeep = <T>(obj: Record<any, any>, path: string): T | null => {
   return (path.split('.').reduce((acc, part) => acc && acc[part], obj) || null) as T | null;
@@ -20,16 +21,30 @@ export const fieldCalculator = (options: { directive: GraphQLDirective }): Compl
       return;
     }
 
-    const extra: Extra | undefined = directiveValues.maxTimes
-      ? {
-          maxTimes: {
-            [`field-${args.type.name}:${args.field.name}`]: {
-              maxTimes: directiveValues.maxTimes,
-              value: 1,
-            },
-          },
-        }
-      : undefined;
+    const extra: Extra = {};
+
+    /**
+     * Services
+     */
+    const services = directiveValues.services as string[] | undefined;
+    if (services) {
+      const servicesObj: ComplexityServices = Object.fromEntries(
+        services.map((serviceName) => [serviceName, { value: 1 }])
+      );
+      extra.services = servicesObj;
+    }
+
+    /**
+     * Max times
+     */
+    if (directiveValues.maxTimes) {
+      extra.maxTimes = {
+        [`field-${args.type.name}:${args.field.name}`]: {
+          maxTimes: directiveValues.maxTimes,
+          value: 1,
+        },
+      };
+    }
 
     const cost = isNumber(directiveValues.cost) ? directiveValues.cost : 0;
     const multiplierString = isString(directiveValues.multiplier) ? directiveValues.multiplier : null;
